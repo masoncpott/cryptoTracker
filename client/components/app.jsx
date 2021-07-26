6472066c41a8a3e9a773b3aa7aa7e6b66b89f7af
Mason Pott <masoncpott@gmail.com>
2021-07-26 11:46:58 -0700

    refactored top level APP component to hooks

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
-class App extends React.Component {
-  constructor (props) {
-    super (props);
-    this.state = {};
-  }
+// class App extends React.Component {
+//   constructor (props) {
+//     super (props);
+//     this.state = {};
+//   }
 
-  async componentDidMount () {
+//   async componentDidMount () {
+//     try {
+//       const response = await axios.get('/testing');
+//       console.log('SUCCESS: ', response)
+//     } catch (e) {
+//       console.log('ERROR: ', e)
+//     }
+//   }
+
+//   render () {
+//     return (
+//       <div>HELLO WORLD</div>
+//     )
+//   }
+// }
+
+function App () {
+  const [selected, setSelected] = useState('');
+
+  useEffect(async () => {
     try {
       const response = await axios.get('/testing');
-      console.log('SUCCESS: ', response)
+      console.log('RESPONSE FROM SERVER:', response);
     } catch (e) {
       console.log('ERROR: ', e)
     }
-  }
+  }, []);
 
-  render () {
-    return (
-      <div>HELLO WORLD</div>
-    )
-  }
+  return (
+    <div>HELLO WORLD</div>
+  )
 }
 
 export default App;
