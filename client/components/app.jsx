3b9e5a8029cda42186fbd53ca854eba0c7cabf1c
Mason Pott <masoncpott@gmail.com>
2021-07-26 11:31:15 -0700

    successfully sending static assets from server

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import axios from 'axios';
 
 class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {};
+  }
+
+  async componentDidMount () {
+    try {
+      const response = await axios.get('/testing');
+      console.log('SUCCESS: ', response)
+    } catch (e) {
+      console.log('ERROR: ', e)
+    }
   }
 
   render () {
