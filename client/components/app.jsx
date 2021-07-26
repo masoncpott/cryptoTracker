e2fe6847bf00bf21c3fed8e90ef5adb7d93ac966
Mason Pott <masoncpott@gmail.com>
2021-07-26 11:54:29 -0700

    clean up and added token

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,28 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
-// class App extends React.Component {
-//   constructor (props) {
-//     super (props);
-//     this.state = {};
-//   }
-
-//   async componentDidMount () {
-//     try {
-//       const response = await axios.get('/testing');
-//       console.log('SUCCESS: ', response)
-//     } catch (e) {
-//       console.log('ERROR: ', e)
-//     }
-//   }
-
-//   render () {
-//     return (
-//       <div>HELLO WORLD</div>
-//     )
-//   }
-// }
-
 function App () {
   const [selected, setSelected] = useState('');
 
